1f0b293b683997be76dc73a7f6bd3088830c2914
Collins Audrey Souop Tagne <t.stacdev@gmail.com>
2024-09-05 17:09:08 +0200

    Project mybook_manager setUp added

---
+++ src/main/java/mybook_manager/MybookManagerApplication.java
@@ -0,0 +1,13 @@
+package mybook_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybookManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybookManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mybook_manager/MybookManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package mybook_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybookManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
